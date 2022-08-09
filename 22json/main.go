ca2b53989ab82f1286fafcd7996c318c0193584c
Abhilash Mallikarjuna <abhilashmallikarjuna2@gmail.com>
2022-08-09 20:13:00 +0530

    Working with JSON in GO

--- 22json/main.go
+++ 22json/main.go
@@ -1,7 +1,73 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type course struct {
+	Name     string `json:"coursename"`
+	Price    int
+	Platform string   `json:"websites"`
+	Password string   `json:"-"`              // ignores this in json output
+	Tags     []string `json:"tags,omitempty"` // no space after comma
+}
 
 func main() {
 	fmt.Println("Welcome to JSON ")
+	// EncodeJson()
+	DecodeJson()
+}
+
+func EncodeJson() {
+	lcoCourses := []course{
+		{"ReactJS Bootcamp", 299, "LearnCodeOnline.in", "abc123", []string{"web-dev", "js"}},
+		{"MERN Bootcamp", 199, "LearnCodeOnline.in", "bcd123", []string{"full-stack", "js"}},
+		{"Angular Bootcamp", 299, "LearnCodeOnline.in", "hit123", nil},
+	}
+
+	// Package this data as JSON
+
+	finalJson, err := json.MarshalIndent(lcoCourses, "", "\t")
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(finalJson))
+}
+
+func DecodeJson() {
+	jsonDatafromWeb := []byte(`
+	{
+		"coursename": "ReactJS Bootcamp",
+		"Price": 299,
+		"websites": "LearnCodeOnline.in",
+		"tags": ["web-dev","js"]
+	}
+	`)
+
+	var lcoCourse course
+
+	checkValid := json.Valid(jsonDatafromWeb)
+
+	if checkValid {
+		fmt.Println("JSON is valid")
+		json.Unmarshal(jsonDatafromWeb, &lcoCourse)
+		fmt.Printf("%#v\n", lcoCourse)
+	} else {
+		fmt.Println("JSON was INVALID")
+	}
+
+	// some cases where you just want add data to a value
+
+	var myOnlineData map[string]interface{}
+
+	json.Unmarshal(jsonDatafromWeb, &myOnlineData)
+	fmt.Printf("%#v\n", myOnlineData)
+
+	for key, value := range myOnlineData {
+		fmt.Println(key, value)
+	}
+
 }
