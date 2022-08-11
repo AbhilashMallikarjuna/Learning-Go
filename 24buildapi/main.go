ac5aef8849339b5a55f9aed023306c08ee908486
Abhilas Mallikarjuna <abhilash.mallikarjuna@go-mmt.com>
2022-08-11 22:06:41 +0530

    Added controllers for course in buildapi section

--- 24buildapi/main.go
+++ 24buildapi/main.go
@@ -1,7 +1,126 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+// models
+type Course struct {
+	CourseId    string  `json:"courseid"`
+	CourseName  string  `json:"coursename"`
+	CoursePrice int     `json:"price"`
+	Author      *Author `json:"author"`
+}
+
+type Author struct {
+	Fullname string `json:"fullname"`
+	Website  string `json:"website"`
+}
+
+// fake DB
+var courses []Course
+
+// middleware
+func (c *Course) IsEmpty() bool {
+	return c.CourseId == "" && c.CourseName == ""
+}
 
 func main() {
 	fmt.Println("Build API")
+
+}
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Welcome to API by learnCodeOnline</h1>"))
+
+}
+
+func getAllCourses(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get all courses")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(courses)
+}
+
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	//get id from request
+	params := mux.Vars(r)
+
+	// loop through courses, find the matching id and return the response
+	for _, course := range courses {
+		if course.CourseId == params["id"] {
+			json.NewEncoder(w).Encode(course)
+		} else {
+			json.NewEncoder(w).Encode("No course found with given id")
+		}
+	}
+	return
+}
+
+func createOneCrouse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Create one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("Please send some data")
+	}
+
+	var course Course
+	_ = json.NewDecoder(r.Body).Decode(&course)
+	if course.IsEmpty() {
+		json.NewEncoder(w).Encode("Please send some data")
+		return
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	course.CourseId = strconv.Itoa(rand.Intn(100))
+	courses = append(courses)
+	json.NewEncoder(w).Encode(course)
+	return
+}
+
+func updateOneCrouse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Update one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	//Grabbing the params
+	params := mux.Vars(r)
+
+	// loop, id , remove , add with id
+
+	for index, course := range courses {
+		if course.CourseId == params["id"] {
+			courses = append(courses[:index], courses[index+1:]...)
+			var course Course
+			_ = json.NewDecoder(r.Body).Decode(&course)
+			course.CourseId = params["id"]
+			courses = append(courses, course)
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+}
+
+func deleteOneCrouse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Deleting one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	//Grabbing the params
+	params := mux.Vars(r)
+
+	// loop, id , remove
+
+	for index, course := range courses {
+		if course.CourseId == params["id"] {
+			courses = append(courses[:index], courses[index+1:]...)
+			break
+		}
+	}
 }
